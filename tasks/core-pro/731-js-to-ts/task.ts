acd260a768d5044f51a65cf3488a6571c488c1c6
Patryk <patryk.kwasek@ing.com>
2026-03-14 13:37:48 +0100

    Core Pro - Finished module 7 - JavaScript - Integracje

--- tasks/core-pro/731-js-to-ts/task.ts
+++ tasks/core-pro/731-js-to-ts/task.ts
@@ -3,6 +3,7 @@ import { getProductCatalog } from './product-catalog.js';
 export type CatalogProducts = ReturnType<typeof getProductCatalog>['products'];
 
 export function getTotalValue() {
-  const catalog = getProductCatalog();
-  return catalog.products.reduce((acc, product) => acc + product.price, 0);
+  const catalog: CatalogProducts = getProductCatalog().products;
+
+  return catalog.reduce((acc: number, product: CatalogProducts[number]) => acc + product.price, 0);
 }
